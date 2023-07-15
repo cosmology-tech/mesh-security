15ea36feefee171f2ad8bc92258e58ba319e092f
Dan Lynch <pyramation@gmail.com>
2023-07-15 04:03:28 +0200

    mesh 🛠

---
+++ README.md
@@ -0,0 +1,56 @@
+# Mesh Security 
+
+<p align="center" width="100%">
+    <img height="90" src="https://user-images.githubusercontent.com/545047/197502724-4cb0f898-116c-4c14-89c8-3358e89eab70.svg" />
+</p>
+
+<p align="center" width="100%">
+   <a href="https://github.com/pyramation/mesh-security/blob/main/LICENSE"><img height="20" src="https://img.shields.io/badge/license-MIT-blue.svg"></a>
+</p>
+
+Typescript libraries for Mesh Security.
+
+## Packages
+
+#### [mesh-security](packages/mesh-security)
+
+TS library with Cosmos SDK and Mesh Security smart contracts.
+
+## Developing
+
+When first cloning the repo:
+
+```
+yarn
+yarn bootstrap
+yarn build
+```
+
+And then get all submodules if necessary:
+
+```
+git submodule update --init
+```
+
+### Building
+
+```sh
+yarn build
+```
+### Publishing
+
+```
+lerna publish
+# lerna publish minor
+# lerna publish major
+```
+## Credits
+
+🛠 Built by Cosmology — if you like our tools, please consider delegating to [our validator ⚛️](https://cosmology.tech/validator)
+
+Code built with the help of these related projects:
+
+* [@cosmwasm/ts-codegen](https://github.com/CosmWasm/ts-codegen) for generated CosmWasm contract Typescript classes
+* [@osmonauts/telescope](https://github.com/osmosis-labs/telescope) a "babel for the Cosmos", Telescope is a TypeScript Transpiler for Cosmos Protobufs.
+* [chain-registry](https://github.com/cosmology/chain-registry) an npm module for the official Cosmos chain-registry
+* [cosmos-kit](https://github.com/cosmology-tech/cosmos-kit) A wallet connector for the Cosmos ⚛️
---
+++ packages/mesh-security/scripts/codegen.js
@@ -0,0 +1,129 @@
+import { join, resolve } from 'path';
+import telescope from '@osmonauts/telescope';
+
+const protoDirs = [join(__dirname, '/../proto')];
+const meshSecurityDir = resolve(
+  join(__dirname, '/../../../mesh-security/contracts')
+);
+
+const contracts = [
+  {
+    name: 'Converter',
+    dir: join(meshSecurityDir, 'consumer', 'converter', 'schema')
+  },
+  {
+    name: 'SimplePriceFeed',
+    dir: join(meshSecurityDir, 'consumer', 'simple-price-feed', 'schema')
+  },
+  {
+    name: 'VirtualStaking',
+    dir: join(meshSecurityDir, 'consumer', 'virtual-staking', 'schema')
+  },
+  // provider
+  {
+    name: 'ExternalStaking',
+    dir: join(meshSecurityDir, 'provider', 'external-staking', 'schema')
+  },
+  {
+    name: 'NativeStaking',
+    dir: join(meshSecurityDir, 'provider', 'native-staking', 'schema')
+  },
+  {
+    name: 'StakingProxy',
+    dir: join(meshSecurityDir, 'provider', 'native-staking-proxy', 'schema')
+  },
+  {
+    name: 'Vault',
+    dir: join(meshSecurityDir, 'provider', 'vault', 'schema')
+  }
+];
+
+telescope({
+  protoDirs,
+  outPath: join(__dirname, '../src/codegen'),
+  options: {
+    prototypes: {
+      excluded: {
+        packages: [
+          'cosmos.auth.v1beta1',
+          'cosmos.app.v1alpha1',
+          'cosmos.app.v1beta1',
+          'cosmos.base.kv.v1beta1',
+          'cosmos.base.reflection.v1beta1',
+          'cosmos.base.snapshots.v1beta1',
+          'cosmos.base.store.v1beta1',
+          'cosmos.base.tendermint.v1beta1',
+          'cosmos.crisis.v1beta1',
+          'cosmos.evidence.v1beta1',
+          'cosmos.feegrant.v1beta1',
+          'cosmos.genutil.v1beta1',
+          'cosmos.group.v1beta1',
+          'cosmos.mint.v1beta1',
+          'cosmos.group.v1',
+          'cosmos.msg.v1',
+          'cosmos.nft.v1beta1',
+          'cosmos.capability.v1beta1',
+          'cosmos.orm.v1alpha1',
+          'cosmos.orm.v1',
+          'cosmos.params.v1beta1',
+          'cosmos.slashing.v1beta1',
+          'cosmos.vesting.v1beta1',
+          'google.api',
+          'ibc.core.port.v1',
+          'ibc.core.types.v1'
+        ]
+      },
+      includePackageVar: false,
+      typingsFormat: {
+        useExact: false,
+        timestamp: 'date',
+        duration: 'duration'
+      }
+    },
+    cosmwasm: {
+      contracts,
+      outPath: join(__dirname, '../src/codegen'),
+      options: {
+        bundle: {
+          enabled: true,
+          bundleFile: 'contracts.ts',
+          scope: 'contracts'
+        },
+        types: {
+          enabled: true
+        },
+        client: {
+          enabled: true
+        },
+        messageComposer: {
+          enabled: true
+        },
+        useContractsHooks: {
+          enabled: true
+        },
+        reactQuery: {
+          enabled: true,
+          version: 'v4',
+          mutations: true
+        }
+      }
+    },
+    aminoEncoding: {
+      enabled: true
+    },
+    lcdClients: {
+      enabled: true
+    },
+    rpcClients: {
+      enabled: true,
+      camelCase: true
+    }
+  }
+})
+  .then(() => {
+    console.log('✨ all done!');
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
